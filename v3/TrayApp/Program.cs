55c63776fa8032a86a8b71fba39c18883cec2516
yixhen <yichenlionelwu@gmail.com>
2024-11-01 22:59:32 -0700

    express server api for chrome extension

--- v3/TrayApp/Program.cs
+++ v3/TrayApp/Program.cs
@@ -15,39 +15,16 @@ namespace TrayApp
         [STAThread]
         static void Main(string[] args)
         {
-            FileLogger appLogger = new FileLogger("log.txt");
-            if (args.Length > 0)
+            Application.EnableVisualStyles();
+            Console.WriteLine("Trayapp is starting");
+            PreAppHandler preAppHandler = new PreAppHandler();
+            ScreenTimeData screenTimeData = preAppHandler.getAndParseFileContents();
+            bool fileToday = preAppHandler.isFileToday(screenTimeData);
+            if (!fileToday)
             {
-                appLogger.log("Started with args");
-                appLogger.log(args[0]);
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                try
-                {
-                    appLogger.log("starting web browser");
-                    process.StartInfo.UseShellExecute = true;
-                    process.StartInfo.FileName = "example.html";
-                    process.Start();
-                }
-                catch (Exception e)
-                {
-                    appLogger.log("error occurred opening web browser");
-                    appLogger.log(e.Message);
-                }
-                appLogger.log("Quitting app");
-                Application.Exit();
-            } else
-            {
-                Application.EnableVisualStyles();
-                Console.WriteLine("Trayapp is starting");
-                PreAppHandler preAppHandler = new PreAppHandler();
-                ScreenTimeData screenTimeData = preAppHandler.getAndParseFileContents();
-                bool fileToday = preAppHandler.isFileToday(screenTimeData);
-                if (!fileToday)
-                {
-                    preAppHandler.moveOldRecord(screenTimeData.day, screenTimeData.month, screenTimeData.year);
-                }
-                Application.Run(new TrayApplicationContext());
+                preAppHandler.moveOldRecord(screenTimeData.day, screenTimeData.month, screenTimeData.year);
             }
+            Application.Run(new TrayApplicationContext());
         }
     }
 }
