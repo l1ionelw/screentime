eee4d875fc3d5a44df06f1717b66dc3a21412ccc
yixhen <143676566+l1ionelw@users.noreply.github.com>
2024-12-20 12:46:24 -0800

    fix error on applicationframehost.exe

--- v3/TrayApp/WindowManager.cs
+++ v3/TrayApp/WindowManager.cs
@@ -13,6 +13,7 @@ namespace TrayApp
 {
     internal class WindowManager
     {
+        static FileLogger logger = new FileLogger("trayapplog.txt", "ScreenTime");
         WinEventDelegate dele = null;
         delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
 
@@ -27,11 +28,21 @@ namespace TrayApp
         
         public static ApplicationInfo getWindowTitle()
         {
-            string applicationPath = UwpUtils.GetProcessFilePath(GetForegroundWindow());
-            string fileDescription = applicationPath != null ? FileVersionInfo.GetVersionInfo(applicationPath).FileDescription : null;
-            string productName = applicationPath != null ? FileVersionInfo.GetVersionInfo(applicationPath).ProductName : null;
-            string appName = fileDescription != null ? fileDescription : applicationPath;
-            appName = appName != null ? appName : "Unknown Application";
+            string applicationPath = null, fileDescription = null, productName = null, appName = null;
+            try
+            {
+                applicationPath = UwpUtils.GetProcessFilePath(GetForegroundWindow());
+                fileDescription = applicationPath != null ? FileVersionInfo.GetVersionInfo(applicationPath).FileDescription : null;
+                productName = applicationPath != null ? FileVersionInfo.GetVersionInfo(applicationPath).ProductName : null;
+                appName = fileDescription != null ? fileDescription : applicationPath;
+                appName = appName != null ? appName : "Unknown Application";
+            }
+            catch (Exception ex)
+            {
+                logger.log("Exception occurred while attempting to get window information");
+                logger.log(ex.Message);
+            }
+            
             ApplicationInfo appInfo = new ApplicationInfo();
             appInfo.path = applicationPath;
             appInfo.productName = productName;
